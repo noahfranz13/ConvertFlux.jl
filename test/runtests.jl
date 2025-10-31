bd8ea94bd9beafab1ee62cfb8341fc87a41e7fe5
Noah Franz <noahfranz13@gmail.com>
2025-10-31 17:57:44 -0500

    first commit for this package!

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,3 +4,11 @@ using Test
 @testset "AstroFluxUtil.jl" begin
     # Write your tests here.
 end
+
+function test_mag_to_flux
+    # some tests here
+end
+
+function test_flux_to_mag
+    # some tests here
+end
